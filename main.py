b32abf2fc1be85eacf5311abe6126c6c6c711e12
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2023-05-15 21:04:27 +0400

    added imports

---
+++ main.py
@@ -0,0 +1,8 @@
+import re
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton, QFileDialog)
+from coding import Encryption as en
+
