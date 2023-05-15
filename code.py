a3741a2f1a672ca6d435487a61383287b7e71f0a
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2023-05-15 20:33:42 +0400

    added Symmetric encryption key decryption function

--- code.py
+++ code.py
@@ -65,3 +65,26 @@ class Encryption:
                 f"{err} error when writing to a file {self.settings['symmetric_key']}")
         else:
             logging.info("The symmetric key is written")
+
+    def __sym_key(self) -> bytes:
+        """
+        Symmetric encryption key decryption function
+
+        Returns the decrypted symmetric key
+        """
+        try:
+            with open(self.settings['private_key'], "rb") as f:
+                private_key = serialization.load_pem_private_key(
+                    f.read(), password=None)
+        except OSError as err:
+            logging.warning(
+                f"{err} error when writing to a file {self.settings['private_key']}")
+        try:
+            with open(self.settings['symmetric_key'], "rb") as f:
+                encrypted_symmetric_key = f.read()
+            symmetric_key = private_key.decrypt(encrypted_symmetric_key, padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+        except OSError as err:
+            logging.warning(
+                f"{err} error when writing to a file {self.settings['symmetric_key']}")
+        return symmetric_key
